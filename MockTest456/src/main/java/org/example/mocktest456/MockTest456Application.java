d080c3aafd0c4cb39d9ea8742c81fe2f1bf4ffd7
XuanHuan55 <166469185+XuanHuan55@users.noreply.github.com>
2025-08-21 21:31:41 +0700

    l8

---
+++ MockTest456/src/main/java/org/example/mocktest456/MockTest456Application.java
@@ -0,0 +1,13 @@
+package org.example.mocktest456;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTest456Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockTest456Application.class, args);
+	}
+
+}
---
+++ MockTest456/src/test/java/org/example/mocktest456/MockTest456ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.mocktest456;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTest456ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
